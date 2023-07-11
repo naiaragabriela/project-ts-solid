82405c073eda45c0235d472db86d5064206824ba
naiaragabriela <naiara.costa@5by5.com.br>
2023-07-11 12:26:07 -0300

    feat: liskov substituton principle

---
+++ src/lsp/main.ts
@@ -0,0 +1,32 @@
+/*
+Liskov substituion principle (principio da substituição de liskov)
+Subtipos precisam ser substituíveis por seus tipos de base
+Ex. Se meu programa espera um animal, algo do tipo cachorro(que herda animal),
+deve servir como qualquer outro animal
+*/
+
+import { Messaging } from './services/messaging';
+import { Order } from './classes/order';
+import { Persistency } from './services/persistency';
+import { Product } from './classes/product';
+import { ShoppingCart } from './classes/shopping-cart';
+import { NoDiscount } from './classes/discount';
+
+//const fiftyPercentDiscount = new FiftyPercentDiscount();
+//const tenPercentDiscount = new TenPercentDiscount();
+const noDiscount = new NoDiscount();
+const shoppingCart = new ShoppingCart(noDiscount);
+const messaging = new Messaging();
+const persistency = new Persistency();
+const order = new Order(shoppingCart, messaging, persistency);
+
+shoppingCart.addItem(new Product('Camiseta', 49.9));
+shoppingCart.addItem(new Product('Caderno', 9.9));
+shoppingCart.addItem(new Product('Lápis', 1.59));
+
+console.log(shoppingCart.items);
+console.log(shoppingCart.total());
+console.log(shoppingCart.totalWithDiscount());
+console.log(order.orderStatus);
+order.checkout();
+console.log(order.orderStatus);
